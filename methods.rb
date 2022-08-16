fd234a9ee0bf5fe2e8529dea772e1ad64eeb5b04
Cecil-Anyega <ceciliaanyega@gmail.com>
2022-08-16 23:15:52 +0300

    commit tree f5e961e3016723968c1c0bc54dd171957bebb986

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer
+    puts"Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end 
+greet"cecil"
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default()    
+greet_with_default("cecil")    
+
+def add (num1, num2)
+    return num1+num2
+end  
+sum1= add(2,5)  
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+    return num/2
+end
+halve(12)    
